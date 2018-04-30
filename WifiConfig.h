54dfc439807e241245b0f74fc9685cfe9f74e588
DrX7FFF <dubourg.v@gmail.com>
2018-04-30 13:20:29 +0200

    Init

---
+++ WifiConfig.h
@@ -0,0 +1,7 @@
+//WifiConfig.h
+
+#ifndef WIFI_CONFIG_H
+#define WIFI_CONFIG_H
+#define YOUR_WIFI_SSID "Mackenzie"
+#define YOUR_WIFI_PASSWD "E5F71122CD9474D4DA1746525E3EAE"
+#endif // !WIFI_CONFIG_H
